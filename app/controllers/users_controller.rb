6ea1b463303d4d6c9c35f5d1eb90eb1efc350356
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-03-15 20:20:48 +0900

    create controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
-  
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.where.not(id: current_user.id)
+    @user = User.find(current_user.id)
   end
 end
