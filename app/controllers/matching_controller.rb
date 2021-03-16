be5a98a1230503dd72d6b9ce7532d61e575d1909
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-03-16 19:43:45 +0900

    add view

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -2,7 +2,7 @@ class MatchingController < ApplicationController
   before_action :authenticate_user!
   def index
     got_reaction_user_ids = Reaction.where(to_user_id: current_user.id, status: 'like').pluck(:from_user_id)
-    @match_users = Reaction.where(to_user_id: got_reaction_users_ids, from_user_id: current_user.id, status: 'like').map(&:to_user)
+    @match_users = Reaction.where(to_user_id: got_reaction_user_ids, from_user_id: current_user.id, status: 'like').map(&:to_user)
     @user =User.find(current_user.id)
   end
 end
