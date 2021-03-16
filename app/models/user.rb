907fb88c2b60da1d49ae1d08781c7d280955f4dd
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-03-16 20:06:11 +0900

    make association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :reactions
+  has_many :chat_rooms, througn: :chat_room_users
+  has_many :chat_room_users
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
