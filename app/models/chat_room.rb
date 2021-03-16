2bc108cfa96badeb6b03deeac8192feace2940ee
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-03-16 21:00:04 +0900

    create chat_room controller

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,4 +1,4 @@
 class ChatRoom < ApplicationRecord
-  has_many :users, througn: :chat_room_users
+  has_many :users, through: :chat_room_users
   has_many :chat_room_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :reactions
-  has_many :chat_rooms, througn: :chat_room_users
+  has_many :chat_rooms, through: :chat_room_users
   has_many :chat_room_users
 
   devise :database_authenticatable, :registerable,
