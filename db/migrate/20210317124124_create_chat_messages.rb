df6b415a8df15ec339b047fbe59a72648abfcc91
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-03-17 21:43:17 +0900

    add migration

--- db/migrate/20210317124124_create_chat_messages.rb
+++ db/migrate/20210317124124_create_chat_messages.rb
@@ -1,6 +1,9 @@
 class CreateChatMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :chat_messages do |t|
+      t.references :chat_room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.text :content
 
       t.timestamps
     end
