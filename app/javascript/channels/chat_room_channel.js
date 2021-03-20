2a849fbfdcbd2239fc6c6859956098579bc9c877
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-03-20 22:26:20 +0900

    criate chat_room

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -1,6 +1,5 @@
 import consumer from "./consumer"
 
-const chatForm = document.getElementById('chat_form');
 
 const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
   connected() {
@@ -12,20 +11,21 @@ const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
   },
 
   received(data) {
-    return alert(data['chat_message']);
+    const chatMessages = document.getElementById('chat-messages');
+    chatMessages.insertAdjacentHTML('beforeend', data['chat_message']);
   },
 
-  speak(chat_message) {
-    return this.perform('speak', { chat_message: chat_message });
+  speak(chat_message, chat_room_id) {
+    return this.perform('speak', { chat_message: chat_message, chat_room_id: chat_room_id });
   }
 });
 
 if(/chat_rooms/.test(location.pathname)) {
   $(document).on("keydown", ".chat-room__message-form_textarea", function(e) {
     if (e.key === "Enter") {
-      appChatRoom.speak(e.target.value);
+      const chat_room_id = $('textarea').data('chat_room_id')
+      appChatRoom.speak(e.target.value, chat_room_id);
       e.target.value = '';
-      console.log(e.target.value);
       e.preventDefault();
     }
   })
